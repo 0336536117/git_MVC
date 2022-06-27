4b876ef8940e4ca5223ba4be6f3c076dc646563d
0336536117 <108274466+0336536117@users.noreply.github.com>
2022-06-27 22:10:31 +0700

    tao ghi tên đạt

--- WEBSITE_MVC/contact.php
+++ WEBSITE_MVC/contact.php
@@ -13,6 +13,7 @@
 				  <div class="contact-form">
 				  	<h2>Contact Us</h2>
 					    <form>
+							<p>dat</p>
 					    	<div>
 						    	<span><label>NAME</label></span>
 						    	<span><input type="text" value=""></span>
